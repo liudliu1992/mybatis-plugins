bad2d63b88b467529fb4d6b70142198ef29160e1
leo <leo.sl.liu1992@gmail.com>
2019-06-14 10:37:31 +0800

    [新增功能](v1.0): 注释插件
    mybatis自定义代码生成器注释插件, 在使用代码生成器生成mybatis代码时使用自己定义的注释格式

---
+++ plugin/src/main/java/com/leo/mybatis/plugins/CustomAnnotationGenerator.java
@@ -0,0 +1,318 @@
+package com.leo.mybatis.plugins;
+
+
+import org.mybatis.generator.api.CommentGenerator;
+import org.mybatis.generator.api.IntrospectedColumn;
+import org.mybatis.generator.api.IntrospectedTable;
+import org.mybatis.generator.api.MyBatisGenerator;
+import org.mybatis.generator.api.dom.java.*;
+import org.mybatis.generator.api.dom.xml.TextElement;
+import org.mybatis.generator.api.dom.xml.XmlElement;
+import org.mybatis.generator.internal.util.StringUtility;
+
+import java.text.SimpleDateFormat;
+import java.time.ZonedDateTime;
+import java.time.format.DateTimeFormatter;
+import java.util.Date;
+import java.util.List;
+import java.util.Properties;
+import java.util.Set;
+
+/**
+ * 自定义代码构建器注释生成器
+ *
+ * @author liusl
+ * @date 2019/1/22-10:37
+ */
+public class CustomAnnotationGenerator implements CommentGenerator {
+    private Properties properties = new Properties();
+    private boolean suppressDate = false;
+    private boolean suppressAllComments = false;
+    private boolean addRemarkComments = false;
+    private SimpleDateFormat dateFormat;
+
+    public CustomAnnotationGenerator() {
+    }
+
+    @Override
+    public void addConfigurationProperties(Properties properties) {
+        this.properties.putAll(properties);
+        this.suppressDate = StringUtility.isTrue(properties.getProperty("suppressDate"));
+        this.suppressAllComments = StringUtility.isTrue(properties.getProperty("suppressAllComments"));
+        this.addRemarkComments = StringUtility.isTrue(properties.getProperty("addRemarkComments"));
+        String dateFormatString = properties.getProperty("dateFormat");
+        if (StringUtility.stringHasValue(dateFormatString)) {
+            this.dateFormat = new SimpleDateFormat(dateFormatString);
+        }
+
+    }
+
+    @Override
+    public void addFieldComment(Field field, IntrospectedTable introspectedTable, IntrospectedColumn introspectedColumn) {
+        if (!this.suppressAllComments) {
+            field.addJavaDocLine("/**");
+            String remarks = introspectedColumn.getRemarks();
+            field.addJavaDocLine(buildComment(remarks));
+            String sb = " * 数据库中字段为: " +
+                    introspectedColumn.getActualColumnName();
+            field.addJavaDocLine(sb);
+            this.addJavadocTag(field, false);
+            field.addJavaDocLine(" */");
+        }
+    }
+
+    @Override
+    public void addFieldComment(Field field, IntrospectedTable introspectedTable) {
+        if (!this.suppressAllComments) {
+            StringBuilder sb = new StringBuilder();
+            field.addJavaDocLine("/**");
+            field.addJavaDocLine(" * ");
+            sb.append(introspectedTable.getFullyQualifiedTable());
+            field.addJavaDocLine(sb.toString());
+            this.addJavadocTag(field, false);
+
+            field.addJavaDocLine(" */");
+        }
+    }
+
+    @Override
+    public void addModelClassComment(TopLevelClass topLevelClass, IntrospectedTable introspectedTable) {
+        if (!this.suppressAllComments && this.addRemarkComments) {
+            topLevelClass.addJavaDocLine("/**");
+            String remarks = introspectedTable.getRemarks();
+            topLevelClass.addJavaDocLine(buildComment(remarks));
+            String sb = " * 数据库表名称为: " + introspectedTable.getFullyQualifiedTable();
+            topLevelClass.addJavaDocLine(sb);
+            topLevelClass.addJavaDocLine(" * @author liusl");
+            topLevelClass.addJavaDocLine(" * @date " + getRunDateString());
+            topLevelClass.addJavaDocLine(" */");
+        }
+    }
+
+    private String buildComment(String remarks) {
+        StringBuilder builder = new StringBuilder();
+        if (this.addRemarkComments && StringUtility.stringHasValue(remarks)) {
+            String[] remarkLines = remarks.split(System.getProperty("line.separator"));
+            int var7 = remarkLines.length;
+            for (int var8 = 0; var8 < var7; ++var8) {
+                String remarkLine = remarkLines[var8];
+                builder.append(" * ").append(remarkLine).append("\n");
+            }
+        }
+        return builder.toString();
+    }
+
+    @Override
+    public void addClassComment(InnerClass innerClass, IntrospectedTable introspectedTable) {
+        addClassComment(innerClass,introspectedTable,false);
+
+    }
+
+    @Override
+    public void addClassComment(InnerClass innerClass, IntrospectedTable introspectedTable, boolean markAsDoNotDelete) {
+        if (!this.suppressAllComments) {
+            StringBuilder sb = new StringBuilder();
+            innerClass.addJavaDocLine("/**");
+            sb.append(" * 数据库表名称为:  ");
+            sb.append(introspectedTable.getFullyQualifiedTable());
+            innerClass.addJavaDocLine(sb.toString());
+            this.addJavadocTag(innerClass, markAsDoNotDelete);
+            innerClass.addJavaDocLine(" * @author liusl");
+            innerClass.addJavaDocLine(" * @date " + getRunDateString());
+            innerClass.addJavaDocLine(" */");
+        }
+    }
+
+
+    @Override
+    public void addEnumComment(InnerEnum innerEnum, IntrospectedTable introspectedTable) {
+        if (!this.suppressAllComments) {
+            StringBuilder sb = new StringBuilder();
+            innerEnum.addJavaDocLine("/**");
+            sb.append(" * This enum corresponds to the database table ");
+            sb.append(introspectedTable.getFullyQualifiedTable());
+            innerEnum.addJavaDocLine(sb.toString());
+            this.addJavadocTag(innerEnum, false);
+            innerEnum.addJavaDocLine(" */");
+        }
+    }
+
+    @Override
+    public void addGetterComment(Method method, IntrospectedTable introspectedTable, IntrospectedColumn introspectedColumn) {
+
+    }
+
+    @Override
+    public void addSetterComment(Method method, IntrospectedTable introspectedTable, IntrospectedColumn introspectedColumn) {
+    }
+
+    @Override
+    public void addGeneralMethodComment(Method method, IntrospectedTable introspectedTable) {
+        if (!this.suppressAllComments) {
+            StringBuilder sb = new StringBuilder();
+            method.addJavaDocLine("/**");
+            sb.append(" * 数据库名称为： ");
+            sb.append(introspectedTable.getFullyQualifiedTable());
+            method.addJavaDocLine(sb.toString());
+
+            List<Parameter> params = method.getParameters();
+            for (Parameter parm : params) {
+                sb.setLength(0);
+                sb.append(" * @param ");
+                sb.append(parm.getName());
+                sb.append(" the value for ").append(parm.getName());
+                method.addJavaDocLine(sb.toString());
+            }
+            sb.setLength(0);
+            sb.append(" * @return ");
+            sb.append(method.getReturnType().getShortName());
+            sb.append(" return type is ").append(method.getReturnType().getShortName());
+            method.addJavaDocLine(sb.toString());
+            this.addJavadocTag(method, false);
+            method.addJavaDocLine(" */");
+        }
+    }
+
+    @Override
+    public void addJavaFileComment(CompilationUnit compilationUnit) {
+
+    }
+
+    @Override
+    public void addComment(XmlElement xmlElement) {
+        if (!this.suppressAllComments) {
+            xmlElement.addElement(new TextElement("<!--"));
+            StringBuilder sb = new StringBuilder();
+            sb.append("  WARNING - ");
+
+            xmlElement.addElement(new TextElement(sb.toString()));
+            String s = this.getDateString();
+            if (s != null) {
+                sb.setLength(0);
+                sb.append("  This element was generated on ");
+                sb.append(s);
+                sb.append('.');
+                xmlElement.addElement(new TextElement(sb.toString()));
+            }
+
+            xmlElement.addElement(new TextElement("-->"));
+        }
+    }
+
+    @Override
+    public void addRootComment(XmlElement xmlElement) {
+
+    }
+
+    @Override
+    public void addGeneralMethodAnnotation(Method method, IntrospectedTable introspectedTable, Set<FullyQualifiedJavaType> imports) {
+        imports.add(new FullyQualifiedJavaType("javax.annotation.Generated"));
+        String comment = "Source Table: " + introspectedTable.getFullyQualifiedTable().toString();
+        method.addAnnotation(this.getGeneratedAnnotation(comment));
+    }
+
+    @Override
+    public void addGeneralMethodAnnotation(Method method, IntrospectedTable introspectedTable, IntrospectedColumn introspectedColumn, Set<FullyQualifiedJavaType> imports) {
+        imports.add(new FullyQualifiedJavaType("javax.annotation.Generated"));
+        String comment = "Source field: " + introspectedTable.getFullyQualifiedTable().toString() + "." + introspectedColumn.getActualColumnName();
+        method.addAnnotation(this.getGeneratedAnnotation(comment));
+    }
+
+    @Override
+    public void addFieldAnnotation(Field field, IntrospectedTable introspectedTable, Set<FullyQualifiedJavaType> imports) {
+        imports.add(new FullyQualifiedJavaType("javax.annotation.Generated"));
+        String comment = "Source Table: " + introspectedTable.getFullyQualifiedTable().toString();
+        field.addAnnotation(this.getGeneratedAnnotation(comment));
+    }
+
+    @Override
+    public void addFieldAnnotation(Field field, IntrospectedTable introspectedTable, IntrospectedColumn introspectedColumn, Set<FullyQualifiedJavaType> imports) {
+        imports.add(new FullyQualifiedJavaType("javax.annotation.Generated"));
+        String comment = "Source field: " + introspectedTable.getFullyQualifiedTable().toString() + "." + introspectedColumn.getActualColumnName();
+        field.addAnnotation(this.getGeneratedAnnotation(comment));
+        if (!this.suppressAllComments && this.addRemarkComments) {
+            String remarks = introspectedColumn.getRemarks();
+            if (this.addRemarkComments && StringUtility.stringHasValue(remarks)) {
+                field.addJavaDocLine("/**");
+                field.addJavaDocLine(" * Database Column Remarks:");
+                String[] remarkLines = remarks.split(System.getProperty("line.separator"));
+                String[] var8 = remarkLines;
+                int var9 = remarkLines.length;
+
+                for (int var10 = 0; var10 < var9; ++var10) {
+                    String remarkLine = var8[var10];
+                    field.addJavaDocLine(" *   " + remarkLine);
+                }
+
+                field.addJavaDocLine(" */");
+            }
+        }
+
+    }
+
+    @Override
+    public void addClassAnnotation(InnerClass innerClass, IntrospectedTable introspectedTable, Set<FullyQualifiedJavaType> imports) {
+        imports.add(new FullyQualifiedJavaType("javax.annotation.Generated"));
+        String comment = "Source Table: " + introspectedTable.getFullyQualifiedTable().toString();
+        innerClass.addAnnotation(this.getGeneratedAnnotation(comment));
+    }
+
+
+    protected void addJavadocTag(JavaElement javaElement, boolean markAsDoNotDelete) {
+
+        StringBuilder sb = new StringBuilder();
+        sb.append(" * ");
+
+        if (markAsDoNotDelete) {
+            sb.append(" do_not_delete_during_merge");
+        }
+        String s = this.getDateString();
+        if (s != null) {
+            sb.append(' ');
+            sb.append(s);
+        }
+        if (sb.length() > 3) {
+            javaElement.addJavaDocLine(sb.toString());
+        }
+    }
+
+    protected String getDateString() {
+        if (this.suppressDate) {
+            return null;
+        } else {
+            return this.dateFormat != null ? this.dateFormat.format(new Date()) : (new Date()).toString();
+        }
+    }
+
+    protected String getRunDateString() {
+        SimpleDateFormat sdf = new SimpleDateFormat("yyyy/MM/dd-HH:mm");
+        return sdf.format(new Date());
+    }
+
+    private String getGeneratedAnnotation(String comment) {
+        StringBuilder buffer = new StringBuilder();
+        buffer.append("@Generated(");
+        if (this.suppressAllComments) {
+            buffer.append('"');
+        } else {
+            buffer.append("value=\"");
+        }
+
+        buffer.append(MyBatisGenerator.class.getName());
+        buffer.append('"');
+        if (!this.suppressDate && !this.suppressAllComments) {
+            buffer.append(", date=\"");
+            buffer.append(DateTimeFormatter.ISO_OFFSET_DATE_TIME.format(ZonedDateTime.now()));
+            buffer.append('"');
+        }
+
+        if (!this.suppressAllComments) {
+            buffer.append(", comments=\"");
+            buffer.append(comment);
+            buffer.append('"');
+        }
+
+        buffer.append(')');
+        return buffer.toString();
+    }
+}
